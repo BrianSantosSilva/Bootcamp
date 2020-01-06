f9805c3ba7e147e9487230aeff8a99cd4def2f67
Brian Santos <briam.a.s.u.s@gmail.com>
2020-01-05 22:00:43 -0300

    continuando modulo10

--- modulo10/src/components/Button/index.js
+++ modulo10/src/components/Button/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActicityIndicator } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { Container, Text } from './styles';
@@ -8,7 +8,7 @@ export default function Button({ children, loading, ...rest }) {
   return (
     <Container {...rest}>
       {loading ? (
-        <ActicityIndicator size="small" color="#fff" />
+        <ActivityIndicator size="small" color="#fff" />
       ) : (
         <Text>{children}</Text>
       )}
